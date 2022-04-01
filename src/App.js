514c3ad866b7a5d1779cd5f1375cc48b65d57b1c
Latif-Ibrahim <ibrahimabdulatif1@gmail.com>
2022-04-01 05:23:30 -0700

    added useNavigate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Home from './pages/Home'
 import About from './pages/About'
-import Blog from './pages/Blog'
+import Blog from './pages/blog/Blog'
 import {Routes, Route, Navigate} from "react-router-dom"
 
 function App() {
